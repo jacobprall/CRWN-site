a8481372e924e9a3233c21a30aa441c7e551f64e
Jacob <prall.jacob@gmail.com>
2021-01-08 13:30:14 -0800

    added firebase database integration

--- src/App.js
+++ src/App.js
@@ -4,21 +4,29 @@ import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.jsx";
 import LoginContainer from "./pages/login/login-container.jsx";
 import { Route, Switch } from "react-router-dom";
-import { auth } from './firebase/firebase.utils';
-import React, { useState, useEffect } from 'react'
-
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  let [currentUser, setCurrentUser] = useState({currentUser: null})
+  let [currentUser, setCurrentUser] = useState({ currentUser: null });
 
   // function returns an unsubscribe function, passing to return as clean up callback
   useEffect(() => {
-    auth.onAuthStateChanged(user => setCurrentUser({currentUser: user}));
-  }, [])
-  console.log(currentUser)
+    auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          setCurrentUser({
+            currentUser: { id: snapShot.id, ...snapShot.data() },
+          });
+        });
+      } else setCurrentUser({ currentUser: userAuth });
+    });
+  }, []);
+  console.log(currentUser);
   return (
     <div>
-      <Header currentUser={currentUser}/>
+      <Header currentUser={currentUser} />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
