3d1b18353bcd95ba183ed74ab14b1e639715f76c
Jacob <prall.jacob@gmail.com>
2021-01-15 13:44:13 -0800

    update

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,7 +3,7 @@ import "./header.scss";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
-import { connect, shallowEqual, useSelector } from "react-redux";
+import { connect } from "react-redux";
 import CartIcon from '../cart-icon/cart-icon.js'
 import CartDropdown from '../cart-dropdown/cart-dropdown'
 function Header({ currentUser, hidden }) {
